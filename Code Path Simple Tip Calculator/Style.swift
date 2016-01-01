46b8260d59db96adda59bce9e45225b4d34d1cfa
bhanson32 <bhanson@umbc.edu>
2015-12-31 23:54:22 -0500

    First Product
    Added UI Views for color.

---
+++ Code Path Simple Tip Calculator/Style.swift
@@ -0,0 +1,14 @@
+//
+//  Style.swift
+//  Code Path Simple Tip Calculator
+//
+//  Created by DrBDHJ on 12/31/15.
+//  Copyright © 2015 DrBDHJ. All rights reserved.
+//
+
+import UIKit
+
+struct Style{
+    
+    
+}
